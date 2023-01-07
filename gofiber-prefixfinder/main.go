1e97d47db5bf0d33601b15ea832d1575bb62de23
Khoi Nguyen <khoi@auditive.io>
2023-01-06 23:36:05 -0800

    Add anagram finder

--- gofiber-prefixfinder/main.go
+++ gofiber-prefixfinder/main.go
@@ -7,19 +7,26 @@ import (
 )
 
 func main() {
-    trie := load("enable1.txt")
+    anagramFinder := loadAnagrams("enable1.txt")
+    trie := loadTrie("enable1.txt")
     engine := pug.New("./views", ".pug")
     app := fiber.New(fiber.Config{
         Views: engine,
     })
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.Render("index", fiber.Map{"Title": "Prefix Word Finder"})
+        return c.Render("index", fiber.Map{"Title": "Word Tools"})
     })
 
-    app.Get("/similar", func(c *fiber.Ctx) error {
+    app.Get("/prefix", func(c *fiber.Ctx) error {
         similarWords := trie.similar(strings.ToLower(c.Query("search")))
         Jade_similarwords(similarWords, c)
+        return nil
+    })
+
+    app.Get("/anagram", func(c *fiber.Ctx) error {
+        anagrams := anagramFinder.find(strings.ToLower(c.Query("search")))
+        Jade_similarwords(anagrams, c)
         return nil
     })
 
